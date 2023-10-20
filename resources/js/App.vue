8d95f15daebb5315dae26c721d2245558fbd997f
emilie <colleyeemilie@hotmail.com>
2023-10-20 10:11:46 +0200

    Affichage des bonnes informations dans les colonnes

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -9,11 +9,22 @@
 
 <template>
     <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Birthday</th>
+        </tr>
         <tr v-for="user in data">
             <td>
                 <a>
                     {{ user.name }}
                 </a>
+            </td>
+            <td>
+                    {{ user.email }}
+            </td>
+            <td>
+                    {{ user.born_at }}
             </td>
         </tr>
     </table>
