bc5bc03971667e7e09ca97c956434692884e15e6
emilie <colleyeemilie@hotmail.com>
2023-10-20 11:42:05 +0200

    Display modal box on click and close it

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {ref} from "vue";
 import TableUsers from "./components/TableUsers.vue";
+const showModal = ref(false);
 
 const data = ref([]);
     axios.get('/api/users').then(function (response) {
@@ -10,5 +11,10 @@ const data = ref([]);
 
 <template>
     <TableUsers :data=data />
+    <button @click="showModal = true">Show Modal</button>
+    <div v-show="showModal">
+        <p> Test </p>
+        <button  @click="showModal = false">OK</button>
+    </div>
 </template>
 
