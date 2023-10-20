f2a445c6b6c3c1e9f48450a1a23c9dc7c7dbf81b
emilie <colleyeemilie@hotmail.com>
2023-10-20 10:01:20 +0200

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import {ref} from "vue";
+    const data = ref([]);
+
+    axios.get('/api/users').then(function (response) {
+         data.value = response.data.data;
+    });
+</script>
+
+<template>
+    <table>
+        <tr v-for="user in data">
+            <td>
+                <a>
+                    {{ user.name }}
+                </a>
+            </td>
+        </tr>
+    </table>
+
+</template>
+
+<style scoped>
+</style>
